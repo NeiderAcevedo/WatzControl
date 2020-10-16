6d439326e48f9f20112944ccdf878b23e750b449
Jhon Neider Acevedo Cárdenas <71908406+NeiderAcevedo@users.noreply.github.com>
2020-10-15 22:14:00 -0500

    css

--- src/Componet/Description/Description_InicioPP.js
+++ src/Componet/Description/Description_InicioPP.js
@@ -7,10 +7,27 @@ function Description_InicioPP() {
   return (
     <div>
       <p className="TexCabeza">
-        Bienvenid@ a WatzControl, la página Web donde podrás calcular el consumo
-        y valor de todo el consumo de electricidad en tu casa.
+        Bienvenid@ a WatzControl, la página Web donde podrás 
+        calcular el consumo y valor de todo el consumo 
+        de electricidad en tu casa.
       </p>
-      <div className="TextoIzq">
+         <div className="problema">
+          <h1>Problema </h1>
+        </div>
+
+      <div className="Texto1"> 
+          El corregimiento Santa Elena pasa  por una crisis energetica en 
+          los ultimos tiempos, esto debido al uso inadecuado por parte de 
+          los habitantes del corregimiento, y por otro lado debido que los 
+          habitantes ya pasan mayor parte de su tiempo en sus casas, ocacionanado 
+          el alto consumo del recurso energetico, y un total a pagar demasiado costoso.
+      </div>
+
+        <div className="vision">
+          <h1>Visión </h1>
+        </div>
+        
+      <div className="Texto2">
         Es una Página WEB para los habitantes del corregimiento santa elena que
         tienen un alto consumo de energía y por ello una pérdida de dinero y
         recursos, se llama WatzControl y es una calculadora personalizada que
