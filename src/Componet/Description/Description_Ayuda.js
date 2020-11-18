868bae9e6a5f7a60499a41fc7ded727ff2f09b69
Jhon Neider Acevedo Cárdenas <71908406+NeiderAcevedo@users.noreply.github.com>
2020-11-18 12:04:11 -0500

    ay

--- src/Componet/Description/Description_Ayuda.js
+++ src/Componet/Description/Description_Ayuda.js
@@ -4,34 +4,34 @@ import { Link } from "react-router-dom";
 
 function Description_Ayuda() {
   return (
-    <div>
+    <div className="BotonesAyuda">
       <button type="button" className="ButtonAyuda">
-      <Link classname="Botones" to="PagesSNuevoP">
-        Soy Nuevo En La Página
+        <Link classname="Botones" to="PagesSNuevoP">
+          Soy Nuevo En La Página
         </Link>
       </button>
 
       <button type="button" className="ButtonAyuda">
-      <Link classname="Botones" to="PagesPorQ">
-        ¿Por Qué Debo Registrarme?
-     </Link>
-      </button>
-
-      <button type="button" className="ButtonAyuda">
-      <Link classname="Botones" to="PagesInformacion">
-        Información De La Página
+        <Link classname="Botones" to="PagesPorQ">
+          ¿Por Qué Debo Registrarme?
         </Link>
       </button>
 
       <button type="button" className="ButtonAyuda">
-      <Link classname="Botones" to="PagesNotiError2">
-        Notificar Error
+        <Link classname="Botones" to="PagesInformacion">
+          Información De La Página
         </Link>
       </button>
 
       <button type="button" className="ButtonAyuda">
-      <Link classname="Botones" to="PagesRecuHistorial">
-        Recuperar Historial
+        <Link classname="Botones" to="PagesNotiError2">
+          Notificar Error
+        </Link>
+      </button>
+
+      <button type="button" className="ButtonAyuda">
+        <Link classname="Botones" to="PagesRecuHistorial">
+          Recuperar Historial
         </Link>
       </button>
     </div>
--- src/Componet/Description/Description_NuevoPag.js
+++ src/Componet/Description/Description_NuevoPag.js
@@ -3,17 +3,19 @@ import "../../styles.css";
 
 function Description_NuevoPag() {
   return (
-    <div>
+    <div className="SoyNuevo">
       <h2 className="NuevoPagH2"> Soy Nuevo En La Página </h2>
-      <p className="NuevoPagP1">
-        Contactanos a través de nuestro correo electrónico
-        (WatzControl@proyect.com),o nuestra de linea le atención al cliente
-        (031) 346687, para darte instrucciones paso a paso.
-      </p>
-      <p className="NuevoPagP2">
-        en este video podrás encontrar toda la información de como iniciar en
-        nuestra página Web.
-      </p>
+      <div className="jp">
+        <p className="NuevoPagP1">
+          Contactanos a través de nuestro correo electrónico
+          (WatzControl@proyect.com),o nuestra de linea le atención al cliente
+          (031) 346687, para darte instrucciones paso a paso.
+        </p>
+        <p className="NuevoPagP2">
+          en este video podrás encontrar toda la información de como iniciar en
+          nuestra página Web.
+        </p>
+      </div>
     </div>
   );
 }
