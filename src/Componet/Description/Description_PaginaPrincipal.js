1734af52b84eaa5d18e9b63bef856f6f26c6b1d1
Jhon Neider Acevedo Cárdenas <71908406+NeiderAcevedo@users.noreply.github.com>
2020-11-21 19:55:50 -0500

    his

--- src/Componet/Description/Description_PaginaPrincipal.js
+++ src/Componet/Description/Description_PaginaPrincipal.js
@@ -13,9 +13,9 @@ function Description_PaginaPrincipal() {
         Ajustes
     </button>
     <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-      <a class="dropdown-item" href="https://kn2hk.csb.app/PagesPPrincipal">Dropdown link</a>
-      <a class="dropdown-item" href="https://kn2hk.csb.app/PagesPPrincipal">Dropdown link</a>
-      <a class="dropdown-item" href="https://kn2hk.csb.app/PagesPPrincipal">Dropdown link</a>
+      <a class="dropdown-item" href="https://kn2hk.csb.app/PagesPPrincipal">Editar Cuenta</a>
+      <a class="dropdown-item" href="https://kn2hk.csb.app/PagesPPrincipal">Historial Mes</a>
+      <a class="dropdown-item" href="https://kn2hk.csb.app/PagesPPrincipal">Historial link</a>
     </div>
   </div>
 
